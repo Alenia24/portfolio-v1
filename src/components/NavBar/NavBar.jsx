882f6e0767d04f043c322874f64ee9409f83d6a5
Alenia24 <alenia.allenn@gmail.com>
2025-05-25 15:44:08 -0400

    Update navbar and design

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,7 +1,30 @@
-import "./NavBar.css"
+import "./NavBar.css";
+
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { Link } from "react-router-dom";
 
 export default function NavBar() {
   return (
-    <div>NavBar</div>
-  )
+    <div>
+      <Navbar expand="lg" className="bg-body-transparent nav-bar" data-bs-theme="dark">
+        <Container className="py-2 px-5">
+          <div className="logo-container">
+            <Nav.Link as={Link} to={"/"} className="logo">A</Nav.Link>
+            <Nav.Link as={Link} to={"/"} className="logo right">A</Nav.Link>
+          </div>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto nav-links">
+              <Nav.Link as={Link} to={"/"}>Home</Nav.Link>
+              <Nav.Link as={Link} to={"/about"}>About</Nav.Link>
+              <Nav.Link as={Link} to={"/projects"}>Projects</Nav.Link>
+              <Nav.Link as={Link} to={"/contact"}>Contact</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
 }
