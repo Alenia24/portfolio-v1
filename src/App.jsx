17e778dc45e4df2991d19bfe1457fb37a40c42b0
Alenia24 <alenia.allenn@gmail.com>
2025-05-25 14:39:20 -0400

    Import pages and add pages to routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route } from "react-router-dom";
 
-import './App.css'
+// Import Pages
+import Homepage from "./pages/Homepage/Homepage.jsx";
+import About from "./pages/About/About.jsx";
+import Projects from "./pages/Projects/Projects.jsx";
+import Contact from "./pages/Contact/Contact.jsx";
+
+import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" />
-        <Route path="/about" />
-        <Route path="/projects" />
-        <Route path="/contact" />
+        <Route path="/" element={<Homepage/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/projects" element={<Projects/>}/>
+        <Route path="/contact" element={<Contact/>}/>
       </Routes>
     </>
   );
 }
 
-export default App
+export default App;
