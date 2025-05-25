54799f20dd4c98eaa711188994022004f1d29a73
Alenia24 <alenia.allenn@gmail.com>
2025-05-25 16:20:13 -0400

    Update and design footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,25 @@
+import { Link } from "react-router-dom"
+
 import "./Footer.css"
+
+import Nav from "react-bootstrap/Nav";
 
 export default function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <div>
+      <hr className="mx-5"  />
+      <div className="footer-container">
+        <div className="footer-logo-container">
+            <Nav.Link as={Link} to={"/"} className="footer-logo">A</Nav.Link>
+            <Nav.Link as={Link} to={"/"} className="footer-logo footer-right">A</Nav.Link>
+        </div>
+        <div className="content">
+          <p>© 2025 Alenia Allen</p>
+        </div>
+        <div className="contact">
+          
+        </div>
+        </div>
+    </div>
+  );
 }
