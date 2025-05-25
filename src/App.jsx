c802528a5b5cdefa5a2c70d9ab4067a14fa315a2
Alenia24 <alenia.allenn@gmail.com>
2025-05-25 14:27:51 -0400

    Create route paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
+import { Routes, Route } from 'react-router-dom'
+
 import './App.css'
 
 function App() {
   return (
     <>
-      
+      <Routes>
+        <Route path="/" />
+        <Route path="/about" />
+        <Route path="/projects" />
+        <Route path="/contact" />
+      </Routes>
     </>
-  )
+  );
 }
 
 export default App
