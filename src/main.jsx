f643d64c5b020cee8483ba97ced6503e4937a525
Alenia24 <alenia.allenn@gmail.com>
2025-05-28 23:58:03 -0400

    Use hashrouter

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App.jsx";
 
 // Import browser router
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
