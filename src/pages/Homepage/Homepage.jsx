122700193767cc621cb86d6fad6eed62d38dc182
Alenia24 <alenia.allenn@gmail.com>
2025-06-01 19:51:40 -0400

    Add about section

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -5,9 +5,20 @@ import NavBar from "../../components/NavBar/NavBar.jsx";
 
 import ProjectItem from "../../components/ProjectItem/ProjectItem.jsx";
 
+import git from "../../assets/git.svg";
+import html from "../../assets/html.png";
+import css from "../../assets/css.png";
+import js from "../../assets/js.png";
+import ts from "../../assets/typescript.png";
+import react from "../../assets/reactlogo.png";
+import tailwind from "../../assets/tailwind.svg";
+import github from "../../assets/github.png";
+import reactrouters from "../../assets/react-router.svg";
+import bootstrap from "../../assets/bootstrap.png";
+import express from "../../assets/express.png";
+import node from "../../assets/node.png";
 import solara from "../../assets/projectimages/solara.png";
 import weather from "../../assets/projectimages/weather.png";
-import dog from "../../assets/projectimages/dog.png";
 
 import photo from "../../assets/me.png";
 import "./Homepage.css";
@@ -42,8 +53,76 @@ export default function Homepage() {
         </div>
       </div>
 
+      <div className="homepage-about-container">
+        <h2 className="homepage-about-title">About Me</h2>
+        <p>
+          Hey! I'm a full stack developer passionate about building functional,
+          user-friendly web apps using technologies like JavaScript, React,
+          Node.js, Express, and MongoDB, with a focus on clean code, thoughtful
+          design, and real-world problem solving.
+        </p>
+        <div className="homepage-about-skills">
+          <div className="homepage-skill">
+            <img src={html} alt="" width={20} height={20} />
+            HTML
+          </div>
+          <div className="homepage-skill">
+            <img src={css} alt="" width={20} height={20} />
+            CSS
+          </div>
+          <div className="homepage-skill">
+            <img src={bootstrap} alt="" width={20} height={20} />
+            Bootstrap
+          </div>
+          <div className="homepage-skill">
+            <img src={tailwind} alt="" width={20} height={20} />
+            Tailwind CSS
+          </div>
+          <div className="homepage-skill">
+            <img src={js} alt="" width={20} height={20} />
+            JavaScript
+          </div>
+          <div className="homepage-skill">
+            <img src={ts} alt="" width={20} height={20} />
+            TypeScript
+          </div>
+          <div className="homepage-skill">
+            <img src={react} alt="" width={20} height={20} />
+            React
+          </div>
+          <div className="homepage-skill">
+            <img src={reactrouters} alt="" width={20} height={20} />
+            React Routers
+          </div>
+          <div className="homepage-skill">
+            <img src={node} alt="" width={20} height={20} />
+            Node.js
+          </div>
+          <div className="homepage-skill">
+            <img src={express} alt="" width={20} height={20} />
+            Express
+          </div>
+          <div className="homepage-skill">
+            <img src={git} alt="" width={20} height={20} />
+            Git
+          </div>
+          <div className="homepage-skill">
+            <img src={github} alt="" width={20} height={20} />
+            Github
+          </div>
+        </div>
+        <button
+          className="view-about-me-btn"
+          onClick={() => {
+            nav("/about");
+          }}
+        >
+          About Me
+        </button>
+      </div>
+
       <div className="homepage-projects-container">
-        <h1 className="homepage-projects-title">Projects</h1>
+        <h2 className="homepage-projects-title">Projects</h2>
         <p>
           Here's a bunch of projects that I've worked on recently, while
           learning, experimenting and solving real world problems.
@@ -81,9 +160,10 @@ export default function Homepage() {
             projectTools={["Html", "CSS", "JavaScript", "API"]}
           />
         </div>
-        <button className="view-all-projects-btn"
+        <button
+          className="view-all-projects-btn"
           onClick={() => {
-            nav("/about");
+            nav("/projects");
           }}
         >
           View All Projects
